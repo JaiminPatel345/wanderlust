b3459c09d0b9952760276c1c5bec95b3b0d8b828
Jaimin Detroja <officialjaimin@gmail.com>
2024-04-01 00:21:07 +0530

    add all routes (backend)

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const methodOverride = require('method-override');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const Listing = require("./models/listing.js");
 
 const port = 3000;
 const app = express();
 
-app.use(express.urlencoded( { extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("views" , path.join(__dirname , 'views'));
-app.set('view engine' , 'ejs');
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 main()
-.then( () => console.log("connection successfully"))
-.catch(err => console.log(err));
+  .then(() => console.log("connection successfully"))
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-
-
-app.get('/' , (req , res) => {
-    res.send("Home Page");
-})
-
-//index route
-app.get('/chats' , async (req , res) => {
-    let chats =  await Chat.find();
-    res.render('index.ejs' , {chats});
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
 
-
-app.get('/chats/new' , async (req , res) => {
-  res.render('new.ejs' );
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("./Listings/index.ejs", { allListings });
 });
 
-// new chat
-app.post('/chats' , async (req , res) => {
-  let {from , to , msg} = req.body;
-  let newChat = new Chat({
-    from : from,
-    to : to,
-    msg : msg,
-    createdAt : new Date()
+app.get("/listings/new", async (req, res) => {
+  res.render("./Listings/new.ejs");
+});
+
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let oneListing = await Listing.findById(id);
+  res.render("./Listings/show.ejs", { listing: oneListing });
+});
+
+app.post("/listings", async (req, res) => {
+  let { title, description, image, price, location, country } = req.body;
+  const newListing = new Listing({
+    title: title,
+    description: description,
+    image: image,
+    price: price,
+    location: location,
+    country: country,
   });
-  newChat.save();
-  res.redirect('/chats');
+
+  await newListing.save();
+  res.redirect("/listings");
 });
 
-
-//edit chat
-app.get('/chats/:id/edit' , async (req , res) => {
-  let {id} = req.params;
-  let chat = await Chat.findById(id);
-  // console.log(chat);
-  res.render('edit.ejs' ,{chat});
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let oneListing = await Listing.findById(id);
+  res.render("./Listings/edit.ejs", { listing: oneListing });
 });
 
-app.put('/chats/:id' , async (req , res) => {
-  let {id} = req.params;
-  let chat = await Chat.findByIdAndUpdate(id , {msg :req.body.msg} , {new : true} , {runValidator : true});
-  
-  res.redirect('/chats') ;
+//update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+
+  Listing.findByIdAndUpdate(id, req.body);
+
+  res.redirect(`/listings/${id}`);
 });
 
-
-app.delete('/chats/:id' , async (req , res) => {
-  let {id} = req.params;
-  await Chat.findByIdAndDelete(id);
-  
-  res.redirect('/chats') ;
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
-
-app.listen(port , () => console.log(`listen on port ${port}`));
+app.listen(port, () => console.log(`listen on port ${port}`));
--- models/listing.js
+++ models/listing.js
@@ -1,36 +1,42 @@
 const mongoose = require('mongoose');
+const default_img = 'https://img.freepik.com/free-vector/wanderlust-explore-adventure-landscape_24908-55313.jpg?w=740&t=st=1711876159~exp=1711876759~hmac=b91c9ca5ccaf8ba289e930b3c62030b97c46e70ad36177b312e199938db1c47c';
 
 const listingSchema = new mongoose.Schema({
     title : {
-        type : string ,
+        type : String ,
         required : true,
         
     },
     description : {
-        type : string ,
+        type : String ,
         
         
     },
     image : {
-        type : string ,
-        required : true,
-        
+        type : String ,
+        default : default_img,
+        set : (v) => v==="" ? default_img : v 
     },
     price : {
-        type : number ,
+        type : Number ,
         required : true,
         
     },
     location : {
-        type : string ,
+        type : String ,
         required : true,
+        set: (v) => v.toUpperCase()
         
     },
     country : {
-        type : string ,
+        type : String ,
+        set: (v) => v.toUpperCase()
         
     }
-})
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports=Listing;
 
 
      
