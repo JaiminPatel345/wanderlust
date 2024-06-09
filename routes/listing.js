0de4882169c1e35937934b6bdfcc68f528cbde81
Jaimin Detroja <officialjaimin345@gmail.com>
2024-06-09 12:16:02 +0530

    create routes folder and separate listing and reviews

---
+++ routes/listing.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const router = express.Router()
+const {listingsSchema } = require('../schema.js')
+const Listing = require('../models/listing.js')
+
+
+//Manage Async errors
+function asyncWrap(fn){
+    return function(req , res , next){
+      fn(req, res, next).catch( (err) => next(err));
+    }
+  }
+
+  class ExpressError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+  
+
+
+  const validateListing = (req , res , next) => {
+    let {error} = listingsSchema.validate(req.body);
+    if(error) {
+      let errMsg =  error.details.map((el) => el.message).join(",");
+      res.status(err.statusCode).render("./Listings/error.ejs", { Swal, err });
+    }else{
+      next();
+    }
+  }
+  
+
+//All listing
+router.get("", async (req, res) => {
+    let allListings = await Listing.find({});
+    res.render("./Listings/index.ejs", { allListings });
+  });
+  
+  //new Listing
+  router.get("/new" , async (req, res) => {
+    res.render("./Listings/new.ejs");
+  });
+  
+  
+  //Show route
+  router.get("/:id", asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id).populate('reviews');
+    res.render("./Listings/show.ejs", { listing });
+  }));
+  
+  
+  //Add route
+  router.post("", validateListing ,  asyncWrap( async (req, res, next) => {
+    
+    
+      let { title, description, image, price, location, country } = req.body;
+      // if(!title || !description || !price || !location || !country) {
+      //   res.status(400).send("Enter all details")
+      // }
+  
+      // let result = listingsSchema.validate(req.body);
+      // console.log(result);
+      
+      const newListing = new Listing({
+        title: title,
+        description: description,
+        image: image,
+        price: price,
+        location: location,
+        country: country,
+      });
+  
+      await newListing.save();
+      res.redirect("/listings");
+    
+  })
+  );
+  
+  
+  //edit route 
+  router.get("/:id/edit", asyncWrap( async (req, res) => {
+    let { id } = req.params;
+    let oneListing = await Listing.findById(id);
+    res.render("./Listings/edit.ejs", { listing: oneListing });
+  })
+  );
+  
+  
+  
+  //update route
+  router.put("/:id", asyncWrap( async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, req.body);
+  
+    res.redirect(`/listings/${id}`);
+  })
+  );
+  
+  
+  
+  //delete route
+  router.delete("/:id", asyncWrap( async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+  );
+  
+
+  module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const router = express.Router({ mergeParams : true})
+const { reviewsSchema} = require('../schema.js')
+const Review = require('../models/review.js')
+const Listing = require('../models/listing.js')
+
+
+//Manage Async errors
+function asyncWrap(fn){
+    return function(req , res , next){
+      fn(req, res, next).catch( (err) => next(err));
+    }
+  }
+
+  class ExpressError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+
+  const validateReview = (req , res , next) => {
+    let {error} = reviewsSchema.validate(req.body);
+    if(error) {
+      let errMsg =  error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400 , errMsg);
+    }else{
+      next();
+    }
+  }
+
+
+//Add review
+router.post("/" , validateReview ,  asyncWrap( async (req , res ) => {
+    let listing = await Listing.findById(req.params.id);
+    let { rating , content} = req.body;
+    let newReview = new Review({
+      content : content,
+      rating : parseInt(rating)
+    });
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+  
+    res.redirect(`/listings/${req.params.id}`)
+  }));
+  
+  
+  
+  //delete review
+  router.delete("/:reviewId" , asyncWrap ( async (req , res) => {
+    let { id , reviewId } = req.params;
+    let listing = await Listing.findById(id);
+    await Review.findByIdAndDelete(reviewId);
+    listing.reviews.pull(reviewId);
+    await listing.save();
+    res.redirect(`/listings/${id}`);
+  })
+  );
+
+
+  module.exports = router;
