9dd929c93cc652392857902fa1c28a4bc80b5a1d
Jaimin Detroja <officialjaimin345@gmail.com>
2024-07-25 16:46:01 +0530

    add price with gst option with backend

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,20 +1,45 @@
 
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+//For tax switch 
+const taxSwitch = document.getElementById("flexSwitchCheckDefault");
+const taxRate = 0.18; // 18% GST
+
+taxSwitch.addEventListener("click", () => {
+  const priceElements = document.querySelectorAll('.card-text span[id^="price-"]');
+
+  for (const priceElement of priceElements) {
+    const originalPrice = parseFloat(priceElement.dataset.originalPrice);
+    const taxAmount = originalPrice * taxRate;
+    const displayedPrice = taxSwitch.checked ? (originalPrice + taxAmount).toLocaleString("en-IN") : originalPrice.toLocaleString("en-IN");
+
+    priceElement.textContent = displayedPrice;
+
+  }
+  let taxInfo = document.getElementsByClassName("tax-info");
+  for (info of taxInfo) {
+    if (info.textContent.includes("Including")) {
+      info.textContent = ` (Excluding GST)`;
+    } else {
+      info.textContent = ` (Including GST)`;
+    }
+  }
+});
