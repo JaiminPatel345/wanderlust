b4f7f8484902e181bbb221e69ea25aadff796fa4
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-11-13 02:40:30 +0530

    vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        // eslint-disable-next-line no-undef
-        target: process.env.VITE_API_BASE_URL, // Use the environment variable
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                // eslint-disable-next-line no-undef
+                target: process.env.VITE_API_BASE_URL, // Use the environment variable
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
     },
-  },
-  define: {
-    'process.env': process.env
-  }
-});
+    define: {
+        "process.env": process.env,
+    },
+})
